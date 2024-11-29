6f24fa43d25d35bec234d11a89bdb0226868d751
fonkou kevin <kevinfonkou09@gmail.com>
2024-11-29 10:18:32 +0100

    premier commite

---
+++ src/main/java/com/ynov/gestion_crise/GestionCriseApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.gestion_crise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/gestion_crise/GestionCriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.gestion_crise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
